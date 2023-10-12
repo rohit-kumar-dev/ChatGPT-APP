330d2434618ea38e7fb28f56985ec997a7e13b46
rohit-kumar-dev <ssrohitmechengg@gmail.com>
2023-10-12 08:50:02 +0530

    ChatGPT HTML structure building

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import send from "./assets/send.svg";
+import user from "./assets/user.png";
+import loader from "./assets/loader.svg";
+import bot from "./assets/loader.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="chatGPT-app">
+      <section className="chat-container">
+        <div className="layout">
+          <div className="chat-bubble">
+            <div className="avatar">
+              <img src={user} />
+            </div>
+            <div className="post">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+              debitis fugit ipsa, ipsam aspernatur magni dicta voluptatem ut
+              sint temporibus obcaecati libero excepturi qui vero nobis
+              voluptatibus id tenetur in.
+            </div>
+          </div>
+          <div className="chat-bubble bot">
+            <div className="avatar">
+              <img src={bot} />
+            </div>
+            <div className="post">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+              debitis fugit ipsa, ipsam aspernatur magni dicta voluptatem ut
+              sint temporibus obcaecati libero excepturi qui vero nobis
+              voluptatibus id tenetur in.
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <input
+          className="composebar"
+          autoFocus
+          type="text"
+          placeholder="Ask Anything!"
+          onChange={() => {}}
+        />
+        <div className="send-button">
+          <img src={send} />
+        </div>
+      </footer>
+    </main>
+  );
 }
 
-export default App
+export default App;
